af174219bcaa7a9554181ef67147e8fc23dac254
Erik on Voyager2 <erik.falor@usu.edu>
2023-09-21 11:25:52 -0600

    Handing this project off to my favorite DuckieCorp intern
                                            ..---..
                                          .'  _    `.
    I just got too busy to            __..'  (o)    :
    complete this, which is a real   `..__          ;
    shame because this will be a          `.       /
    fun program to work on :'(              ;      `..---...___
                                          .'                   `~-. .-')
                                         .                         ' _.'
    At least I know it is in good       :                           :
    hands now!                          \                           '
                                         +                         J
                                          `._                   _.'
                                             `~--....___...---~' mh

---
+++ src/tt.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+#              Copyright © 2023 DuckieCorp. All Rights Reserved.
+#
+#                       __      Redistribution and use of this code, with or
+#                     /` ,\__   without modification, are permitted provided
+#                    |    ).-'  that the following conditions are met:
+#                   / .--'
+#                  / /          0. Redistributions of this code must retain
+#    ,      _.==''`  \             the above copyright notice, this list of
+#  .'(  _.='         |             conditions and the following disclaimer.
+# {   ``  _.='       |          1. The name of the author may not be used to
+#  {    \`     ;    /              endorse or promote products derived from
+#   `.   `'=..'  .='               this software without specific prior written
+#     `=._    .='                  permission.
+#  jgs  '-`\\`__                2. Neither the name of the University nor the
+#           `-._{                  names of its contributors may be used to
+#                                  endorse or promote products derived from
+#                                  this software without specific prior written
+#                                  permission.
+
+
+import sys
+
+from Concatenate import cat, tac
+from CutPaste import cut, paste
+from Grep import grep
+from Partial import head, tail
+from Sorting import sort
+from WordCount import wc
+from Usage import usage
+
+
+if len(sys.argv) < 2:
+    usage()
+    sys.exit(1)
+else:
+    print("TODO: Determine which tool the user has invoked by examining sys.argv")
+    print("TODO: Use if/elif/else to select which function to call")
+    print("TODO: Call the requested tool, passing remaining arguments from sys.argv")
+    print("TODO: Call usage() and exit when bad input is provided")
+
